a64ad7472a1ae4272dcbdbf85f91bd1726805afd
Stefanov, Tihomir (k20051726) <tihomir.stefanov@kcl.ac.uk>
2021-09-09 14:12:39 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularForms-ReactiveForms';
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  //To create a form group we call the group method. The FormBuilder is a simpler way to create formGroups and
+  //formControls. Inside the array of every formControl, the second field is the validation rule.
+  registrationForm = this.formBuilder.group({
+    userName: ['', [Validators.required, Validators.minLength(3)]],
+    password: [''],
+    confirmPassword: [''],
+    address: this.formBuilder.group({
+      city: [''],
+      state: [''],
+      postcode: ['']
+    })
+  });
+
+  get userName() {
+    return this.registrationForm.get('userName');
+  }
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Tihomir'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postcode: new FormControl('')
+  //   })
+  // });
+  //
+  loadApi() {
+    // Set value sets all values inside the form, but is very strict and requires every single value to have a
+    // set value and is not null.
+    // this.registrationForm.setValue({
+
+    //With patchValue we are able to pick and choose which value we want to set values to some of the form controls
+    this.registrationForm.patchValue({
+      userName: 'Bibi',
+      password: 'test',
+      confirmPassword: 'test'
+      // address: {
+      //   city: 'London',
+      //   state: 'Bexley',
+      //   postcode: 'IG35TN'
+      // }
+    });
+  }
 }
